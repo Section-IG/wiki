9abde61fb89bec79279b6c469aea10fc5ac8afc1
Tinaël Devresse <32441291+HunteRoi@users.noreply.github.com>
2022-06-17 17:52:40 +0200

    Update index.js

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -7,6 +7,9 @@ export default function Img({ alt, src, className }) {
 	if (!className) {
 		className = styles.centeredImage;
 	}
+	
+	if (!src.startsWith("/")) src = `/${src}`;
+	if (!src.startsWith("/wiki")) src = `/wiki${src}`;
 
 	return <Image className={className} img={src} alt={alt} />;
 }
