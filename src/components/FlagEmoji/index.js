385297ddd20931a02b5409e172b7bbad89c5397d
Tinaël Devresse <32441291+HunteRoi@users.noreply.github.com>
2022-06-17 18:01:39 +0200

    Update index.js

--- src/components/FlagEmoji/index.js
+++ src/components/FlagEmoji/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import styles from './styles.module.css';
+import Img from '../Image';
 
 const whitelist = ['flag_fr', 'flag_gb'];
 
 export default function FlagEmoji({ name }) {
 	if (!whitelist.includes(name)) name = 'logo';
 
-	return <img src={`/img/${name}`} className={styles.emoji} alt={name} />;
+	return <Img src={`/img/${name}`} className={styles.emoji} alt={name} />;
 }
